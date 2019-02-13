38ca852632ae691876198b9734c7e5c6825e8360
Asmaa Mirkhan <45575570+asmaamirkhan@users.noreply.github.com>
2019-02-13 23:20:39 +0300

    POST and GET requests added

--- hello-world/routes/index.js
+++ hello-world/routes/index.js
@@ -2,8 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.end(JSON.stringify({mesaj: "Selam dunya"}));
+});
+
+router.post('/postMethod', function(req, res) {
+  res.end(JSON.stringify(req.body));
+  console.log(req.body);
+});
+
+router.get('/getMethod', function(req, res) {
+  res.end(JSON.stringify({name: req.query.name, surname: req.query.surname}));
 });
 
 module.exports = router;
