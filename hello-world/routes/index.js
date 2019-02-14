5834396b4915ede52cc90e75f512663eec13f318
Asmaa Mirkhan <45575570+asmaamirkhan@users.noreply.github.com>
2019-02-14 11:37:10 +0300

    Connection to database

--- hello-world/routes/index.js
+++ hello-world/routes/index.js
@@ -1,6 +1,13 @@
 var express = require('express');
+var mysql = require('mysql');
 var router = express.Router();
 
+var conn = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "library"
+});
 
 router.get('/', function(req, res) {
   res.end(JSON.stringify({mesaj: "Selam dunya"}));
@@ -13,6 +20,19 @@ router.post('/postMethod', function(req, res) {
 
 router.get('/getMethod', function(req, res) {
   res.end(JSON.stringify({name: req.query.name, surname: req.query.surname}));
+});
+
+router.get('/bookList', function(req, res) {
+  conn.connect(function(err) {
+    if (err) throw err;
+    console.log("Successfully connected to DB!");
+    var sql = "select * from book_info";
+    conn.query(sql, function (err, result) {
+      if (err) throw err;
+      res.end(JSON.stringify(result));
+    });
+  });
+  
 });
 
 module.exports = router;
