ede7a5fe9c74993d7026c40c12f2c91def497ce8
Asmaa Mirkhan <45575570+asmaamirkhan@users.noreply.github.com>
2019-02-21 13:12:35 +0300

    Exchange service added

--- hello-world/routes/index.js
+++ hello-world/routes/index.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var mysql = require('mysql');
+var express = require("express");
+var mysql = require("mysql");
+const rp = require("request-promise");
 var router = express.Router();
 
 var conn = mysql.createConnection({
@@ -11,40 +12,58 @@ var conn = mysql.createConnection({
 
 conn.connect(function(err) {
   if (err) throw err;
-    console.log("Successfully connected to DB!");
+  console.log("Successfully connected to DB!");
 });
 
-
-router.get('/', function(req, res) {
-  res.end(JSON.stringify({mesaj: "Selam dunya"}));
+router.get("/", function(req, res) {
+  res.end(JSON.stringify({ mesaj: "Selam dunya" }));
 });
 
-router.post('/postMethod', function(req, res) {
+router.post("/postMethod", function(req, res) {
   res.end(JSON.stringify(req.body));
 });
 
-router.get('/getMethod', function(req, res) {
-  res.end(JSON.stringify({name: req.query.name, surname: req.query.surname}));
+router.get("/getMethod", function(req, res) {
+  res.end(JSON.stringify({ name: req.query.name, surname: req.query.surname }));
 });
 
-router.get('/bookList', function(req, res) {
-    var sql = "select * from book_info";
-    conn.query(sql, function (err, result) {
-      if (err) throw err;
-      res.end(JSON.stringify(result));
-    });
+router.get("/bookList", function(req, res) {
+  var sql = "select * from book_info";
+  conn.query(sql, function(err, result) {
+    if (err) throw err;
+    res.end(JSON.stringify(result));
+  });
 });
 
-router.post('/login', function(req, res) {
-    var sql = "select * from users where email = ? and password = ?;";
-    conn.query(sql, [req.body.email, req.body.password],function (err, result) {
-      if (err) throw err;
-      if(result.length)
-        res.end(JSON.stringify({mesaj: "Hoş geldiniz", giris: true}));
-      else
-        res.end(JSON.stringify({mesaj: "Deneme başarısız", giris: false}));
+router.post("/login", function(req, res) {
+  var sql = "select * from users where email = ? and password = ?;";
+  conn.query(sql, [req.body.email, req.body.password], function(err, result) {
+    if (err) throw err;
+    if (result.length)
+      res.end(JSON.stringify({ mesaj: "Hoş geldiniz", giris: true }));
+    else res.end(JSON.stringify({ mesaj: "Deneme başarısız", giris: false }));
+  });
+});
+
+
+const url = "http://www.floatrates.com/daily/try.json";
+
+router.get("/getTRY", function(req, res) {
+  rp(url)
+    .then(function(data) {
+      //let dd = JSON.parse(data).usd.inverseRate;
+      res.end(
+        JSON.stringify({
+          dolar: JSON.parse(data).usd.inverseRate,
+          euro: JSON.parse(data).eur.inverseRate,
+          suriye_lirası: JSON.parse(data).syp.inverseRate,
+          urdun_dinari: JSON.parse(data).jod.inverseRate
+        })
+      );
+    })
+    .catch(function(err) {
+      console.log(err);
     });
-  
 });
 
 module.exports = router;
